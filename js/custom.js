34a26ad4a632448380ec38f44c4e6b491bf1c5fc
staley7 <staleylance@gmail.com>
2013-02-14 10:11:31 -0600

    Toggle buttons working
    need to position them correctly

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,24 @@
 /* javascript file for the custom.html page*/
 
 
+
+function(){
+	document.getElementById("source1").toggleClass("down");
+}
+
+/*$(document).ready(function(){
+	document.getElementById("source1").click(function(){
+		$(this).toggleClass("down");
+	});
+});
 //javascript calls to jquery call to create toogle button
 $(document).ready(function(){
-    $('a#button').click(function(){
+    $('div.source1').click(function(){
         $(this).toggleClass("down");
     });
 });
+
+function toggle(button)
+{
+	
+ }
